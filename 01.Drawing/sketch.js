40d186dc27b3fac8ba26c2d3d2754f692700dda7
alaslett <amylaslett12@icloud.com>
2025-10-12 21:53:25 +0100

    Finished sketch

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -5,6 +5,9 @@ function setup() {
   background ('rgba(0, 150, 255, 0.3)'); 
   
   describe ('A canvas with a transparent blue background');
+}
+
+  function draw() {
   
   // Create a p5.Color object using RGB values. 
   let c = color('lightgrey');
@@ -24,7 +27,7 @@ function setup() {
   //Top-right
   point(280, 337);
   
-  //Bottom-left
+  //Bottom-leftj
   point(420, 337);
   
   // Style the points.
@@ -47,9 +50,97 @@ line (320, 445, 350, 425);
 
 line(350, 425, 350, 447); 
 
-line(350,447, 380, 425);
+line(350, 447, 380, 425);
 
 describe ('Three black lines creating a small zig-zag within the light grey circle');
 
-}
+textSize(50);
+noStroke();
+fill('red');
+text('?', 325, 210);
+
+describe('A question mark in red above the lightgrey circle');
+
+noFill();
+stroke('purple');
+strokeWeight(4);
+ellipse(380, 210, 10, 15);
+
+describe('A transparent small purple ellipse');
+
+// Style the line.
+stroke('magenta');
+strokeWeight(4)
+line (360, 230, 380, 250);
+
+//Style the line.
+stroke('cyan');
+strokeWeight(4)
+line(360, 250, 380, 230);
+
+
+
+
+
+//Left
+stroke ('Green');
+strokeWeight(4);
+line (370, 220, 370, 199);
+
+//Right
+stroke('orange');
+strokeWeight(4);
+line (390, 220, 390, 200);
+
+describe('A green and orange line accompanied by a small purple circle');
+
+textSize(25);
+noStroke();
+fill('Blue');
+text('01100011', 210, 250);
+
+describe('binary for the letter c in blue'); 
+
+
+textSize(17);
+stroke('green');
+strokeWeight(2)
+fill('green');
+text('01101111', 260, 253);
+
+describe('binary for the letter o in green'); 
+
+textSize(15);
+stroke('black');
+strokeWeight(1)
+fill('pink');
+text('01000100', 350, 245);
+
+describe('binary for the letter d in pink'); 
+
+textSize(21);
+stroke('black');
+strokeWeight(1)
+fill('orange');
+text('01000101', 335, 190);
+
+describe('binary for the letter e in pink'); 
+
+textSize(15);
+fill('purple');
+text('01100011011011110100010001000101', 240, 210);
+describe('binary for code in purple'); 
+
+// Top 
+stroke('yellow');
+strokeWeight(5);
+line (270, 200, 300, 200);
+
+// Bottom
+stroke('Hotpink');
+strokeWeight(5);
+line (270, 210, 300, 210);
+
+
+  }
   
